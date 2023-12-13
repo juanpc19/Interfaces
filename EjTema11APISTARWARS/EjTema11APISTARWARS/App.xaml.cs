65f1009c96fee87117adb7bc46c7f1537baf9fd5
juanpc19 <juan.perez@iesnervion.es>
2023-12-13 23:10:22 +0100

    pullpushpullpush`puillspush

---
+++ EjTema11APISTARWARS/EjTema11APISTARWARS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjTema11APISTARWARS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EjTema11APISTARWARS/EjTema11APISTARWARS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EjTema11APISTARWARS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
