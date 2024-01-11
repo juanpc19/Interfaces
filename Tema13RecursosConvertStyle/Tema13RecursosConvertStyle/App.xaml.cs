59f4d883035eaf637c5bd594440080ff7f4b88d0
juanpc19 <juan.perez@iesnervion.es>
2024-01-11 13:19:14 +0100

    a

---
+++ Tema13RecursosConvertStyle/Tema13RecursosConvertStyle/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tema13RecursosConvertStyle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Tema13RecursosConvertStyle/Tema13RecursosConvertStyle/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema13RecursosConvertStyle
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
